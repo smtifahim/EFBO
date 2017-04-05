6613fd02e278c0e6c2dcaf0257c8eca453c958c5
smtifahim <smtifahim@gmail.com>
2017-04-05 06:44:04 -0400

    Added testing for individual field values for Annotation objects.

--- EFBO/src/ca/queensu/efbo/annotation/extractor/Tester.java
+++ EFBO/src/ca/queensu/efbo/annotation/extractor/Tester.java
@@ -1,11 +1,25 @@
 package ca.queensu.efbo.annotation.extractor;
 
+import java.util.ArrayList;
+
 public class Tester {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		AnnotationExtractor annot = new AnnotationExtractor();
+		ArrayList <Annotation> annotations = annot.getExtractedAnnotations();
+		
+		for (Annotation annotation: annotations)
+		{
+			System.out.println("\n\n");
+			System.out.println(annotation.getFileLocation());
+			System.out.println(annotation.getLineNumber());
+			System.out.println(annotation.getSubject());
+			System.out.println(annotation.getPredicate());
+			System.out.println(annotation.getObject());
+		}
+			
 		System.out.println(annot.getExtractedAnnotations());
 	}
 
